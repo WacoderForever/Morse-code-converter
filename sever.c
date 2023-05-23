091e93e746b2713de07b6299995d98cc4a3eea85
WacoderForever <sethomondi088@gmail.com>
2023-05-23 11:29:43 +0300

    res

--- sever.c
+++ sever.c
@@ -1,18 +1,19 @@
 #include "dependencies/CWebStudio.h"
 #include "dependencies/doTheWorld.h"
-#include "eng_morse.c"
+#include "morse/english_to_morse.c"
+#include "morse/morse_to_english.c"
 
 #include "interfaces/render_interface.c"
 #include "public_routes/main_route.c"
-#include "public_routes/morse_route.c"
+#include "public_routes/translate_route.c"
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
     request->read_content(request,2000);
 
     char *route = request->route;
     //when short is clicked
-    if(strcmp(route,"/morse_route") == 0) {
-        return morse_route(request);
+    if(strcmp(route,"/translate_route") == 0) {
+        return translate_route(request);
     }
     
     return main_route();
